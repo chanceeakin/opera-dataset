3b0867e16c0600a31e3e92851d9934793426cfe7
Chance Eakin <chance.eakin@gmail.com>
2019-01-05 14:29:59 -0600

    ugh linux casing

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-import Nav from './Nav'
+import Nav from './nav'
 
 class Header extends React.Component {
   render() {
