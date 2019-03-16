64830dfe3fe2f18db779d712ce26b2e9adc81433
Chance Eakin <chance.eakin@gmail.com>
2019-03-15 21:01:24 -0500

    new chart

--- client/src/components/charts/voice-types-by-year.js
+++ client/src/components/charts/voice-types-by-year.js
@@ -2,116 +2,164 @@ import React from 'react'
 import { Group } from '@vx/group'
 import { BarGroup } from '@vx/shape'
 import { AxisBottom } from '@vx/axis'
-import { cityTemperature } from '@vx/mock-data'
 import { scaleBand, scaleLinear, scaleOrdinal } from '@vx/scale'
-import { timeParse, timeFormat } from 'd3-time-format'
+import { PARTS } from '../../constants'
+import { withTooltip, Tooltip } from '@vx/tooltip'
+import json from '../../../../data/multi-year.json'
 
 const blue = '#aeeef8'
 const green = '#e5fd3d'
-const purple = '#9caff6'
 const bg = '#612efb'
 
-const data = cityTemperature.slice(0, 8)
-const keys = Object.keys(data[0]).filter(d => d !== 'date')
+const newData = PARTS.map(part => {
+  const reduced = json.data.reduce((acc, curr) => {
+    acc['Voice Type'] = part
+    if (curr['Voice Type'] === part) {
+      acc[curr['Year']] = curr['Frequency']
+    }
+    return acc
+  }, {})
+  return reduced
+})
 
-const parseDate = timeParse('%Y%m%d')
-const format = timeFormat('%b %d')
-const formatDate = date => format(parseDate(date))
+const newKeys = Object.keys(newData[0]).filter(d => d !== 'Voice Type')
 
 // accessors
-const x0 = d => d.date
+const x0 = d => d['Voice Type']
 
 // scales
 const x0Scale = scaleBand({
-  domain: data.map(x0),
+  domain: newData.map(x0),
   padding: 0.2,
 })
 const x1Scale = scaleBand({
-  domain: keys,
+  domain: newKeys,
   padding: 0.1,
 })
 const yScale = scaleLinear({
-  domain: [0, Math.max(...data.map(d => Math.max(...keys.map(key => d[key]))))],
+  domain: [
+    0,
+    Math.max(...newData.map(d => Math.max(...newKeys.map(key => d[key])))),
+  ],
 })
 const color = scaleOrdinal({
-  domain: keys,
-  range: [blue, green, purple],
+  domain: newKeys,
+  range: [blue, green],
 })
 
-export default ({
-  width,
-  height,
-  margin = {
-    top: 40,
-  },
-}) => {
-  // bounds
-  const xMax = width
-  const yMax = height - margin.top - 100
+export default React.memo(
+  withTooltip(
+    ({
+      width,
+      height,
+      margin = {
+        top: 40,
+      },
+      tooltipOpen,
+      tooltipLeft,
+      tooltipTop,
+      tooltipData,
+      tooltipTimeout,
+      hideTooltip,
+      showTooltip,
+    }) => {
+      // bounds
+      const xMax = width
+      const yMax = height - margin.top - 100
 
-  x0Scale.rangeRound([0, xMax])
-  x1Scale.rangeRound([0, x0Scale.bandwidth()])
-  yScale.range([yMax, 0])
+      x0Scale.rangeRound([0, xMax])
+      x1Scale.rangeRound([0, x0Scale.bandwidth()])
+      yScale.range([yMax, 0])
 
-  return (
-    <svg width={width} height={height}>
-      <rect x={0} y={0} width={width} height={height} fill={bg} rx={14} />
-      <Group top={margin.top}>
-        <BarGroup
-          data={data}
-          keys={keys}
-          height={yMax}
-          x0={x0}
-          x0Scale={x0Scale}
-          x1Scale={x1Scale}
-          yScale={yScale}
-          color={color}
-        >
-          {data => {
-            return data.map(barGroup => {
-              return (
-                <Group
-                  key={`bar-group-${barGroup.index}-${barGroup.x0}`}
-                  left={barGroup.x0}
-                >
-                  {barGroup.bars.map(bar => {
+      return (
+        <React.Fragment>
+          {tooltipOpen && (
+            <Tooltip
+              top={tooltipTop}
+              left={tooltipLeft}
+              style={{
+                minWidth: 60,
+                backgroundColor: 'rgba(0,0,0,0.9)',
+                color: 'white',
+              }}
+            >
+              <div style={{ color: color(tooltipData.key) }}>
+                <strong>{tooltipData.key}</strong>
+              </div>
+              <div>{tooltipData.value}</div>
+            </Tooltip>
+          )}
+          <svg width={width} height={height}>
+            <rect x={0} y={0} width={width} height={height} fill={bg} rx={14} />
+            <Group top={margin.top}>
+              <BarGroup
+                data={newData}
+                keys={newKeys}
+                height={yMax}
+                x0={x0}
+                x0Scale={x0Scale}
+                x1Scale={x1Scale}
+                yScale={yScale}
+                color={color}
+              >
+                {data => {
+                  return data.map(barGroup => {
                     return (
-                      <rect
-                        key={`bar-group-bar-${barGroup.index}-${bar.index}-${
-                          bar.value
-                        }-${bar.key}`}
-                        x={bar.x}
-                        y={bar.y}
-                        width={bar.width}
-                        height={bar.height}
-                        fill={bar.color}
-                        rx={4}
-                        onClick={event => {
-                          const { key, value } = bar
-                          alert(JSON.stringify({ key, value }))
-                        }}
-                      />
+                      <Group
+                        key={`bar-group-${barGroup.index}-${barGroup.x0}`}
+                        left={barGroup.x0}
+                      >
+                        {barGroup.bars.map(bar => {
+                          return (
+                            <rect
+                              key={`bar-group-bar-${barGroup.index}-${
+                                bar.index
+                              }-${bar.value}-${bar.key}`}
+                              x={bar.x}
+                              y={bar.y}
+                              width={bar.width}
+                              height={bar.height}
+                              fill={bar.color}
+                              rx={4}
+                              onMouseLeave={() => {
+                                tooltipTimeout = setTimeout(() => {
+                                  hideTooltip()
+                                }, 200)
+                              }}
+                              onMouseEnter={e => {
+                                if (tooltipTimeout) clearTimeout(tooltipTimeout)
+                                const top = e.clientY - margin.top - bar.height
+                                const left = bar.x + bar.width + 60
+                                showTooltip({
+                                  tooltipData: bar,
+                                  tooltipTop: top,
+                                  tooltipLeft: left,
+                                })
+                              }}
+                            />
+                          )
+                        })}
+                      </Group>
                     )
-                  })}
-                </Group>
-              )
-            })
-          }}
-        </BarGroup>
-      </Group>
-      <AxisBottom
-        top={yMax + margin.top}
-        tickFormat={formatDate}
-        scale={x0Scale}
-        stroke={green}
-        tickStroke={green}
-        hideAxisLine={true}
-        tickLabelProps={(value, index) => ({
-          fill: green,
-          fontSize: 11,
-          textAnchor: 'middle',
-        })}
-      />
-    </svg>
+                  })
+                }}
+              </BarGroup>
+            </Group>
+            <AxisBottom
+              top={yMax + margin.top}
+              scale={x0Scale}
+              stroke={green}
+              tickStroke={green}
+              hideAxisLine={true}
+              tickLabelProps={() => ({
+                fill: green,
+                fontSize: 11,
+                textAnchor: 'middle',
+              })}
+            />
+          </svg>
+        </React.Fragment>
+      )
+    }
   )
-}
+)
